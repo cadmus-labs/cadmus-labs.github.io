2cb950e5b38c2080480529eacf9feaa705fe5c6e
Nadun De Silva <nadunrds@gmail.com>
2023-07-29 00:07:21 +1200

    Add website oracle GitHub actions

--- pages/solutions/website-oracle.tsx
+++ pages/solutions/website-oracle.tsx
@@ -33,72 +33,103 @@ import SectionHeading from "@/components/content/SectionHeading";
 import { Photos } from "@/constants/images";
 import { Logos } from "@/constants/logos";
 
-const Overview = (): React.ReactElement => (
-    <Section>
-        <Photo float={"right"} photo={Photos.AnalyzePhoto} />
-        <Paragraph>
-            Welcome to Website Oracle, a cutting-edge API developed by Cadmus
-            Labs. Website Oracle offers a comprehensive set of utility endpoints
-            designed to empower you with in-depth analysis and insights into
-            various aspects of websites deployed on the internet.
-        </Paragraph>
-        <Paragraph>
-            With Website Oracle, you gain access to a wide array of features
-            that enable you to delve deep into websites and gain a thorough
-            understanding of their structure and dependencies. Our API is your
-            trusted ally in gaining valuable insights into website elements for
-            various use cases.
-        </Paragraph>
-        <Paragraph>
-            Please ensure that you adhere to our Terms of Use while using the
-            Website Oracle API. By using our API, you acknowledge your
-            responsibility to conduct lawful analyses and assessments in
-            compliance with the laws governing the websites you evaluate. Cadmus
-            Labs does not assume any liability for the usage of the API beyond
-            the scope of the agreed Terms.
-        </Paragraph>
-        <SectionHeading>Key Features</SectionHeading>
-        <ul>
-            <li>
-                Analyze Dependencies and Broken Links: Discover the hidden
-                intricacies of your website&apos;s dependency graph and easily
-                identify broken links that may be hampering user experience. Our
-                endpoint for analyzing dependencies provides valuable insights
-                into how your website&apos;s various components interact.
-            </li>
-            <li>
-                Retrieve SSL Certificates with Ease: Obtain essential SSL
-                certificate-related information for any website of your choice
-                through our intuitive API endpoint. Decipher certificates
-                effortlessly and access crucial details to enhance your
-                website&apos;s security and credibility.
-            </li>
-            <li>
-                Get Website Content in Plain Text: Access the content of any
-                website in plain text format through our new API endpoint.
-                Simplify data extraction and analysis by retrieving the textual
-                content of web pages for further processing and insights.
-            </li>
-        </ul>
-        <SectionHeading>Support</SectionHeading>
-        <Paragraph>
-            If you require assistance or have any inquiries regarding the
-            Website Oracle API, we&apos;re here to help! Please feel free to
-            reach out to us through the Rapid API Support channel. Our dedicated
-            support team will promptly address your questions and provide the
-            necessary guidance to ensure a smooth and successful experience with
-            our API.
-        </Paragraph>
-        <SectionHeading>Pricing</SectionHeading>
-        <Paragraph>
-            Please refer to the pricing section for detailed information about
-            the available pricing plans. Each plan includes sensible usage
-            limits to cater to various needs. Additionally, we offer the
-            flexibility to create custom private plans based on the requests,
-            tailored to specific requirements.
-        </Paragraph>
-    </Section>
-);
+const Overview = (): React.ReactElement => {
+    const checkWebsiteDependencies = (
+        <Link
+            href="https://github.com/marketplace/actions/check-website-dependencies"
+            target="_blank"
+        >
+            Check Website Dependencies
+        </Link>
+    );
+    const checkWebsiteCertificate = (
+        <Link
+            href="https://github.com/marketplace/actions/check-website-certificate"
+            target="_blank"
+        >
+            Check Website Certificate
+        </Link>
+    );
+    return (
+        <Section>
+            <Photo float={"right"} photo={Photos.AnalyzePhoto} />
+            <Paragraph>
+                Welcome to Website Oracle, a cutting-edge API developed by
+                Cadmus Labs. Website Oracle offers a comprehensive set of
+                utility endpoints designed to empower you with in-depth analysis
+                and insights into various aspects of websites deployed on the
+                internet.
+            </Paragraph>
+            <Paragraph>
+                With Website Oracle, you gain access to a wide array of features
+                that enable you to delve deep into websites and gain a thorough
+                understanding of their structure and dependencies. Our API is
+                your trusted ally in gaining valuable insights into website
+                elements for various use cases.
+            </Paragraph>
+            <Paragraph>
+                Please ensure that you adhere to our Terms of Use while using
+                the Website Oracle API. By using our API, you acknowledge your
+                responsibility to conduct lawful analyses and assessments in
+                compliance with the laws governing the websites you evaluate.
+                Cadmus Labs does not assume any liability for the usage of the
+                API beyond the scope of the agreed Terms.
+            </Paragraph>
+            <SectionHeading>Key Features</SectionHeading>
+            <ul>
+                <li>
+                    Analyze Dependencies and Broken Links: Discover the hidden
+                    intricacies of your website&apos;s dependency graph and
+                    easily identify broken links that may be hampering user
+                    experience. Our endpoint for analyzing dependencies provides
+                    valuable insights into how your website&apos;s various
+                    components interact.
+                </li>
+                <li>
+                    Retrieve SSL Certificates with Ease: Obtain essential SSL
+                    certificate-related information for any website of your
+                    choice through our intuitive API endpoint. Decipher
+                    certificates effortlessly and access crucial details to
+                    enhance your website&apos;s security and credibility.
+                </li>
+                <li>
+                    Get Website Content in Plain Text: Access the content of any
+                    website in plain text format through our new API endpoint.
+                    Simplify data extraction and analysis by retrieving the
+                    textual content of web pages for further processing and
+                    insights.
+                </li>
+            </ul>
+            <SectionHeading>Eco System</SectionHeading>
+            <ul>
+                <li>
+                    GitHub Actions
+                    <ul>
+                        <li>{checkWebsiteDependencies}</li>
+                        <li>{checkWebsiteCertificate}</li>
+                    </ul>
+                </li>
+            </ul>
+            <SectionHeading>Support</SectionHeading>
+            <Paragraph>
+                If you require assistance or have any inquiries regarding the
+                Website Oracle API, we&apos;re here to help! Please feel free to
+                reach out to us through the Rapid API Support channel. Our
+                dedicated support team will promptly address your questions and
+                provide the necessary guidance to ensure a smooth and successful
+                experience with our API.
+            </Paragraph>
+            <SectionHeading>Pricing</SectionHeading>
+            <Paragraph>
+                Please refer to the pricing section for detailed information
+                about the available pricing plans. Each plan includes sensible
+                usage limits to cater to various needs. Additionally, we offer
+                the flexibility to create custom private plans based on the
+                requests, tailored to specific requirements.
+            </Paragraph>
+        </Section>
+    );
+};
 
 const TermsOfUse = (): React.ReactElement => {
     const cadmusLabsEmail = (
